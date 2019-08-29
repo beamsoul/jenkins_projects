99cc387ec33e507a39cf0333b5ade3131e5f5fbb
beamsoul <nurzhanasamidinova@gmail.com>
2019-08-28 18:15:52 -0700

    add

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,20 +5,17 @@ node{
     }
     stage("Install git"){
         sh "ssh  ec2-user@${ENVIR} sudo yum install git python-pip -y"
-       
-
     }
     stage("Pull Repo"){
-        sh "ssh  ec2-user@${ENVIR} git clone https://github.com/farrukh90/stormpath-flask-sample.git 2< /dev/null"
+        sh "ssh  ec2-user@${ENVIR} git clone https://github.com/farrukh90/stormpath-flask-sample.git 2> /dev/null"
     }
     stage("Install Requirements"){
-
         sh "echo Hello"
     }
     stage("Pip Install"){
         sh "ssh  ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/stormpath-flask-sample/requirements.txt"
     }
     stage("Run App"){
-        sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/stormpath-flask-sample/bootstrap.py"
+        sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/stormpath-flask-sample/app.py"
     }
 }
---
+++ re
@@ -0,0 +1,1 @@
+properties([parameters([string(defaultValue: '18.222.222.138', description: 'please provide IP', name: 'ENVIR', trim: true)])])
